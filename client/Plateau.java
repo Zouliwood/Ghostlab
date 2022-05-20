8b52097066424c4921eef54f75e6ddbb031998c4
david <david2maykel@gmail.com>
2022-05-20 23:49:56 +0200

    patch map block

--- client/Plateau.java
+++ client/Plateau.java
@@ -14,20 +14,20 @@ public class Plateau {
                 plateau[i][j]=0;
             }
         }
-        this.player=new Joueur(initX, initY);
+        this.player= new Joueur(initX, initY);
     }
 
    void addWall(int direction, int nbrCase, int newX, int newY){
         player.updatePos(newX, newY);
         if (player.isBlocked(direction, nbrCase)){
             if (direction==0){//haut
-                if (player.y+1<plateau.length)this.plateau[player.x][player.y+1]=2;
+                if (player.y+1<plateau.length)this.plateau[player.y+1][player.x]=2;
             } else if (direction==1) {//droite
-                if (player.x+1<plateau[0].length)this.plateau[player.x+1][player.y]=2;
+                if (player.x+1<plateau[0].length)this.plateau[player.y][player.x+1]=2;
             } else if (direction==2) {//bas
-                if (player.y-1>=0)this.plateau[player.x][player.y+1]=2;
+                if (player.y-1>=0)this.plateau[player.y-1][player.x]=2;
             }else{//gauche
-                if (player.x-1>=0)this.plateau[player.x-1][player.y]=2;
+                if (player.x-1>=0)this.plateau[player.y][player.x-1]=2;
             }
         }
    }
@@ -36,14 +36,14 @@ public class Plateau {
         for (int i = plateau.length-1; i>=0; i--) {
             for (int j = 0; j < plateau[0].length; j++) {
                 if (player.y==i && player.x==j){
-                    System.out.print(" X ");
+                    System.out.print("X ");
                 }else System.out.print((plateau[i][j]==2)?"\u2B1B ":"\u2B1C ");
             }
-            System.out.println("");
+            System.out.println();
         }
     }
 
-    private class Joueur{
+    private static class Joueur{
         private int oldx, oldy;
         private int x, y;//val courrante de retour serv
 
@@ -56,13 +56,14 @@ public class Plateau {
 
         boolean isBlocked(int direction, int nbrCase){
             if (direction==0){//haut
-                return oldy+nbrCase<y;
+                System.out.println("_"+oldy+"_"+nbrCase+"_"+y+"_");
+                return oldy+nbrCase>y;
             }else if (direction==1){//droite
-                return oldx+nbrCase<x;
+                return oldx+nbrCase>x;
             }else if (direction==2){//bas
-                return oldy-nbrCase>y;
+                return oldy-nbrCase<y;
             }else{//gauche
-                return oldx-nbrCase>x;
+                return oldx-nbrCase<x;
             }
         }
         public void updatePos(int x, int y){
@@ -73,7 +74,7 @@ public class Plateau {
         }
     }
 
-    private class Fantome {
+    private static class Fantome {
         private int x, y;
 
         Fantome(int x, int y){
